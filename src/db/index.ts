06e2cc094af2dfbc7677e87eadb175ae8aafd2a6
MRM Abdullah <mrmabdullah2024@gmail.com>
2026-05-28 06:21:39 -0400

    l7a2 exam setup and server create and database create

---
+++ src/db/index.ts
@@ -0,0 +1,54 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+
+            role VARCHAR(20)
+            CHECK(role IN ('contributor', 'maintainer'))
+            DEFAULT 'contributor',
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        );
+    `);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues(
+            id SERIAL PRIMARY KEY,
+
+            title VARCHAR(150) NOT NULL,
+
+            description TEXT NOT NULL
+            CHECK(LENGTH(description) >= 20),
+
+            type VARCHAR(30)
+            CHECK(type IN ('bug', 'feature_request'))
+            NOT NULL,
+
+            status VARCHAR(30)
+            CHECK(status IN ('open', 'in_progress', 'resolved'))
+            DEFAULT 'open',
+
+            reporter_id INT NOT NULL,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        );
+    `);
+
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
